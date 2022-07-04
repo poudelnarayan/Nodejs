1a0c48293c3ba641f2bf5fa1f441f63b0b268e0a
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-04 19:31:24 +0545

    redirecting requests

--- NodeBasic/app.js
+++ NodeBasic/app.js
@@ -4,10 +4,25 @@
 
 
 const http = require('http');
-
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers,);
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type = "text"  name="message"> <button type = "submit" >Submit</button></button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'SUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
