db5c0485a48dcc4eeabde0c9f94f6c9147d0cbbe
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-06 22:32:43 +0545

    using express router

--- StartingWithExpress/app.js
+++ StartingWithExpress/app.js
@@ -3,21 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method ="POST"><input type = "text" name = "title"><button type="submit">Add product</button> </form>');
-});
 
-// executes only for a post request
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from express</h1>');
-});
 
 app.listen(3000);
