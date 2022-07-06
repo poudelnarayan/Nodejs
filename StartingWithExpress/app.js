9a9cc039267bee9c4211fc4bc8a314b180624ee3
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-06 22:16:37 +0545

    parsing incoming request

--- StartingWithExpress/app.js
+++ StartingWithExpress/app.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-})
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In another the middleWare');
-    res.send('<h1>Add Product Page</h1>');
+    res.send('<form action="/product" method ="POST"><input type = "text" name = "title"><button type="submit">Add product</button> </form>');
 });
 
+app.use('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+
+})
 
 app.use('/', (req, res, next) => {
-    console.log('In another the middleWare');
     res.send('<h1>Hello from express</h1>');
 });
 
