351a92cc7a69dd446f943ea5630cbd5feaea20c9
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-09 17:52:44 +0545

    working with ejs

--- WorkingWithDynamicContent/app.js
+++ WorkingWithDynamicContent/app.js
@@ -1,27 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs.engine({ extname: '.hbs', layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' }));
+// app.engine('hbs', expressHbs.engine({ extname: '.hbs', layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' }));
 
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+// app.set('view engine', 'hbs');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTItle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "error" });
 });
 
 app.listen(3000);
