fb2cd2f15ba930517f3b8e1d48184eaccbcd9508
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-10 19:47:31 +0545

    Adding products controllers

--- WorkingWithDynamicContent/routes/admin.js
+++ WorkingWithDynamicContent/routes/admin.js
@@ -1,28 +1,18 @@
-const path = require("path");
+// const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const productsController = require("../controllers/products");
+// const rootDir = require("../util/path");
 
 const router = express.Router();
 
-const products = [];
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-});
+router.get("/add-product", productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
+router.post("/add-product", productsController.postAddProduct);
 
-  res.redirect("/");
-});
+module.exports = router;
 
-// module.exports = router;
-exports.routes = router;
-exports.products = products;
