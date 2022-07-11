fbe526d9a650bad4056e424cd0c18dc6c9047989
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-11 09:33:35 +0545

    storing and fetching data in and from files

--- WorkingWithDynamicContent/models/product.js
+++ WorkingWithDynamicContent/models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const dirPath = require("../util/path");
+
+// const products = [];
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +10,29 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this); // this refers to the object created at this instant
+    const p = path.join(dirPath, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+      console.log(fileContent);
+    });
+    // products.push(this); // this refers to the object created at this instant
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(dirPath, "data", "products.json");
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
