97be9c06de826c45caaa3896f262024a0d7b4ee5
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-11 13:14:24 +0545

    registering routes

--- WorkingWithDynamicContent/controllers/admin.js
+++ WorkingWithDynamicContent/controllers/admin.js
@@ -17,10 +17,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop/product-list", {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
   });
 };
