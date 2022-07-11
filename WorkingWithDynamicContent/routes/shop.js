83002cd5cee46d189739d472627e70fc6c7d0b90
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-11 20:26:23 +0545

    extracting dynamic parameters

--- WorkingWithDynamicContent/routes/shop.js
+++ WorkingWithDynamicContent/routes/shop.js
@@ -10,6 +10,18 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
+// ':' tells that there will be some value and that will be set in productId
+router.get("/products/:productId", shopController.getProduct);
+// example: /products/3434
+
+/*
+If there would be a route such as:
+router.get("/products/delete") 
+then it must be placed aboe the dynamic route handler as the code flows from top to bottom , the more precise route 
+will reach first , else 'delete' would be considerd dynamic and will be set into productId.
+
+*/
+
 router.get("/cart", shopController.getCart);
 
 router.get("/orders", shopController.getOrders);
