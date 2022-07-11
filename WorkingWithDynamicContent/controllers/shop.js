5053b509002c1cc80585066368ee128cf171329f
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-11 20:51:34 +0545

    rendering the product details view

--- WorkingWithDynamicContent/controllers/shop.js
+++ WorkingWithDynamicContent/controllers/shop.js
@@ -12,8 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId; // productId , because in the dynamic router we use this name after colon ':'
-  console.log(prodId);
-  res.redirect('/');
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
