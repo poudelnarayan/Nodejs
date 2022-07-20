37cba470072eda4309961b1ae49018199dc4ffea
Narayan Poudel <poudelnarayan434@gmail.com>
2022-07-20 20:23:09 +0545

    creating and fetching the cart

--- WorkingWithDynamicContent/app.js
+++ WorkingWithDynamicContent/app.js
@@ -49,7 +49,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
     // console.log(result);
@@ -65,6 +66,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
